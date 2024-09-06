40550ebcaeeed35d11bd7ebb06b03ae2a1f65417
Gabrielli-Rech <gabbyrech133@gmail.com>
2024-09-06 09:23:01 -0300

    ArenaDeObjetos

--- src/arenadeobjetos/ArenaDeObjetos.java
+++ src/arenadeobjetos/ArenaDeObjetos.java
@@ -1,10 +1,8 @@
 package arenadeobjetos;
-
 import java.util.Scanner;
-
 public class ArenaDeObjetos {
     static Scanner ler = new Scanner(System.in);
-    public static int LerInt (){
+    public static int LerInt() {
         Scanner ler = new Scanner(System.in);
         int num = 99;
         try {
@@ -16,11 +14,26 @@ public class ArenaDeObjetos {
         return num;
     }
     public static void main(String[] args) {
-        String texto;
-        int num;
-        System.out.print("Digite seu nome: ");
-        texto = ler.nextLine();
-        System.out.print("Informe sua idade: ");
-        num = LerInt();
+    }
+    public static void menuP() {
+        String menu;
+        menu = "0 - Sair\n"
+                + "1 - Criar Personagens \n"
+                + "2 - Listar Personagens \n"
+                + "3 - Batalha";
+        System.out.println(menu);
+    }
+    public static void subMenuP(int omP) {
+        String subMenu;
+        switch (omP) {
+            case 1 :
+                break;
+            case 2 :
+                break;
+            case 3 :
+                break;
+            default:
+                break;
+        }
     }
 }
