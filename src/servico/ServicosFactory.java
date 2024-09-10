c3abad8e26ba5ec78379d82c27afb2781609ed06
Gabrielli-Rech <gabbyrech133@gmail.com>
2024-09-10 11:38:08 -0300

    arena

--- src/servico/ServicosFactory.java
+++ src/servico/ServicosFactory.java
@@ -2,7 +2,8 @@ package servico;
 
 public class ServicosFactory {
     private static VitimasServicos VitimaS = new VitimasServicos();
-    public static VitimasServicos getVitimasServicos (){
+
+    public static VitimasServicos getVitimasServicos() {
         return VitimaS;
     }
 }
--- src/servico/VitimasServicos.java
+++ src/servico/VitimasServicos.java
@@ -3,6 +3,7 @@ package servico;
 import dao.DAOFactory;
 import dao.VitimaDAO;
 import model.Pessoa;
+
 public class VitimasServicos {
     public void cadastrarVitima(Pessoa vVO) {
         VitimaDAO vDAO = DAOFactory.geVitimaDAO();
