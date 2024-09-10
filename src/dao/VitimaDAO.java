dceaec9b18009b5db10bceec3c94fed015830b18
Gabrielli-Rech <gabbyrech133@gmail.com>
2024-09-10 09:57:22 -0300

    arena

--- src/dao/VitimaDAO.java
+++ src/dao/VitimaDAO.java
@@ -6,21 +6,20 @@ import java.sql.SQLException;
 
 import conexao.Conexao;
 import model.Pessoa;
-public class PessoaDAO {
-    public void cadastrarPessoa(Pessoa pVO) {
+public class VitimaDAO {
+    public void cadastrarVitima(Pessoa vVO) {
         try {
             Connection con = Conexao.getConexao();
             // id, nome, olho, cabelo, pele, sexo, armamento, planoDeFuga, pontosDeVida
             String sql = "Insert into pessoa values" + "null, ?, ?, ?, ?, ?, null, null, ?";
             PreparedStatement pst = con.prepareStatement(sql);
-            pst.setString(1, pVO.getNome());
-            pst.setString(2, pVO.getOlho());
-            pst.setString(3, pVO.getCabelo());
-            pst.setString(4, pVO.getPele());
-            pst.setBoolean(5, pVO.isSexo());
-            pst.setString(6, pVO.getNome());
-            pst.setString(7, pVO.getNome());
-            pst.setInt(8, pVO.getPontosDeVida());
+            pst.setString(1, vVO.getNome());
+            pst.setString(2, vVO.getOlho());
+            pst.setString(3, vVO.getCabelo());
+            pst.setString(4, vVO.getPele());
+            pst.setBoolean(5, vVO.isSexo());
+            pst.setInt(6, vVO.getPontosDeVida());
+            pst.execute();
         } catch (SQLException e) {
             System.out.println("Erro ao cadastrar pessoa" + e.getMessage());
         }
